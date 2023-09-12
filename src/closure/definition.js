6a553cd39a158abf3d90c2f5cc8ba8db3f9e90e5
Jaka Ocampo <jakaocampo@Jakas-MBP.ip>
2023-09-12 17:32:41 +1200

    closures definitions and concepts

---
+++ src/closure/definition.js
@@ -0,0 +1,14 @@
+/*
+Permiten acceder al ambito de una funcion exterior
+desde una funcion interior. En JavaScript, las clausuras
+se crean cada vez que una funcion es creada.
+
+A diferencia de otros conceptos como funciones, 
+variables u otros. Los closures no son siempre
+utilizados.
+
+Tienes una closure cuando una funcion cualquiera
+accede a una variable fuera de su contexto.
+
+
+*/
