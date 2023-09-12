b281e0ddfd0117415763eb2a6e6de19296af5b81
Jaka Ocampo <jakaocampo@Jakas-MBP.ip>
2023-09-12 15:45:55 +1200

    block and reasign files concepts

---
+++ src/scope/re.js
@@ -0,0 +1,30 @@
+var firstName; // Undefined
+firstName = 'Oscar';
+console.log(firstName);
+
+var lastName = 'David'; // Declarar / asignar
+lastName = 'Ana'; // Reasignar
+console.log(lastName);
+
+var secondName = 'David'; // declarando / asignando
+var secondName = 'Ana'; // reasignando
+console.log(secondName);
+
+// Let
+let fruit = 'Apple'; // declarar y asignar
+fruit = 'Kiwi'; // reasignar
+console.log(fruit);
+
+let fruit = 'Banana'; // no se puede redeclarar
+console.log(fruit);
+
+// const
+const animal = 'dog' // declara y asigna
+animal = 'cat' // No se puede reasignar
+
+const vehicles = [];
+vehicles.push('vwGolf');
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
