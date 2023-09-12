7544ac1caab36e2709534674c62b95b8cebebdfb
Jaka Ocampo <jakaocampo@Jakas-MBP.ip>
2023-09-12 17:28:18 +1200

    strict definition

---
+++ src/scope/strict.js
@@ -0,0 +1,10 @@
+'use strict'; // I have to declare the variable first
+pi = 3.1416;
+console.log(pi);
+
+function myFunc() {
+    'use strict';
+    return pi = 3.1416;
+}
+
+console.log(myFunc());
