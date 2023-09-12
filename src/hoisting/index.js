4acfcd5c920f200ef8c4cd95c7a3901a7722f8df
Jaka Ocampo <jakaocampo@Jakas-MBP.ip>
2023-09-12 17:49:57 +1200

    hoisting definition

---
+++ src/hoisting/index.js
@@ -0,0 +1,17 @@
+//Primero se declarala variable pero se le 
+//asigna el valor indefined por eso no puede 
+//mostrar el nombre que le asignamos a la variable nameOfDog
+/*console.log(nameOfDog); 
+var nameOfDog = 'Elmo';
+console.log(nameOfDog);
+*/
+
+//Esto es lo que sucede en esta funcion
+//var elmo; que define como undefined
+nameOfDog();
+
+function nameOfDog() {
+    console.log(`El mejor perrito es ${elmo}`);
+}
+
+var elmo = 'Elmo';
